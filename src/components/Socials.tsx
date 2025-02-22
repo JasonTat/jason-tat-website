6f1a6e3aee4aad5eca4e7305873dfdfa0b911a81
JasonTat <112578835+JasonTat@users.noreply.github.com>
2025-02-22 16:38:24 -0700

    minor commit - updated socials and added a little to projects area. not done

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,20 +1,28 @@
 import LinkedIn from "../../public/icons/2977346dcf8ee744bea80c79f503b44c.png"
 import Github from "../../public/icons/946af03d916e146c2a87dfcf6116b163.png";
+import Email from "../../public/icons/email-icon.png";
 import Image from "next/image";
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 
 const myLinkedInUrl = "https://www.linkedin.com/in/jason-tat-4b66b3140/";
 const myGithubUrl = "https://github.com/JasonTat";
-
+const emailAddress = "jason.t123@live.ca"
 
 const Socials = () => {
     return (
-        <div className="flex flex-row ">
-            <a href={myLinkedInUrl}>
+        <div className="flex flex-row">
+            <a className="mr-2" href={myLinkedInUrl}>
                 <Image src={LinkedIn} alt="LinkedIn" width={35} height={35}/>
             </a>
-            <a href={myGithubUrl}>
+            <a className="mr-2 ml-2" href={myGithubUrl}>
                 <Image src={Github} alt="LinkedIn" width={35} height={35}/>
+            </a>
+            <a className="ml-2" href={`mailto:${emailAddress}`}>
+                <Image src={Email} alt="Email" width={35} height={40} />
             </a>
 
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,29 +24,36 @@ export default function Home() {
           I'm a Full Stack Developer with a passion for creating excellent and innovative projects. 
         </p>
         <p className="">
-          I've always had a passion for technology, especially software. The first time I've ever read, touched, and written code was back in 2010 during the peak era of Tumblr blogs, where I'd edit the HTML and CSS of my own blog page.  
+          I've always had a passion for technology, especially software. I grew up with computers and video games; the first time I've ever read, touched, and written code was back in 2010 during the peak era of Tumblr blogs, where I'd edit the HTML and CSS of my own blog page.  
         </p>
         <p className="mt-6">
-          What got me interested in software was working at TELUS as a Technical Support Representative. We'd use multiple pieces of software to analyze and diagnose a client's issue, and it was through doing that, that I became aware of how amazing software can be, and interested how it looked like under the hood.
+          What got me interested in pursuing software was working at TELUS as a Technical Support Representative. We'd use multiple pieces of software to analyze and diagnose a client's issue, and it was through doing that, that I became aware of how amazing software can be, and interested in how it looked and functioned under the hood, especially in an intricately connected system.
         </p>
       </div>
 
       {/* education */}
       <div className="mt-6 text-xl w-1/2">
-
+        <p className="italic font-semibold mb-2">Education</p>
         <p>
-          I graduated with honours from Northern Alberta Institute of Technology (NAIT) with a 3.8 GPA. I'm extremely grateful for my time at NAIT, for the instructors that taught me. 
-          Their passion for technology, development, and career development inspires me to continue my own learning and career development in this field. 
+          I graduated with honours from Northern Alberta Institute of Technology (NAIT) with a 3.8 GPA. I'm extremely grateful for my time at NAIT, and for the instructors that taught me. 
+          Their passion for technology, development, and career development inspired me to continue my own learning and career development in this field. 
         </p>
       </div>
 
       {/* projects / experience */}
       <div className="mt-6 text-xl w-1/2">
-        <h2 className="italic font-semibold">  
+        <h2 className="italic font-semibold mb-2">  
           Projects
         </h2>
         {/* PROJECTS component */}
+        <p className="mt-3 ">
+          Assistive Devices 3D Printing Library
+          <span className="block italic">
+            Client: Alberta Health Services
+          </span> 
+          <span className="">Role: Software Developer Team Lead</span>
 
+        </p>
 
       </div>
       {/* socials */}
