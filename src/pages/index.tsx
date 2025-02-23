305c8a26094afae6ec63af79648f280b183c6717
JasonTat <112578835+JasonTat@users.noreply.github.com>
2025-02-22 17:03:39 -0700

    projects update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const fredoka = Fredoka({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
   return (
-
+    // DESKTOP    
     <div className={`${fredoka.className} pt-16 text-slate-700 flex flex-col items-center justify-center`}>
       
       {/* intro */}
@@ -19,7 +19,7 @@ export default function Home() {
       </div>
       {/* about me */}
       <div className="mt-6 text-xl w-1/2">
-        <p className="italic font-semibold mb-2">A little about me</p>
+        <p className="italic text-2xl font-semibold mb-2">A little about me</p>
         <p className="mb-6">
           I'm a Full Stack Developer with a passion for creating excellent and innovative projects. 
         </p>
@@ -29,11 +29,11 @@ export default function Home() {
         <p className="mt-6">
           What got me interested in pursuing software was working at TELUS as a Technical Support Representative. We'd use multiple pieces of software to analyze and diagnose a client's issue, and it was through doing that, that I became aware of how amazing software can be, and interested in how it looked and functioned under the hood, especially in an intricately connected system.
         </p>
-      </div>
+      </div>``
 
       {/* education */}
       <div className="mt-6 text-xl w-1/2">
-        <p className="italic font-semibold mb-2">Education</p>
+        <p className="italic text-2xl font-semibold mb-2">Education</p>
         <p>
           I graduated with honours from Northern Alberta Institute of Technology (NAIT) with a 3.8 GPA. I'm extremely grateful for my time at NAIT, and for the instructors that taught me. 
           Their passion for technology, development, and career development inspired me to continue my own learning and career development in this field. 
@@ -42,19 +42,34 @@ export default function Home() {
 
       {/* projects / experience */}
       <div className="mt-6 text-xl w-1/2">
-        <h2 className="italic font-semibold mb-2">  
+        <h2 className="italic text-2xl font-semibold mb-2">  
           Projects
         </h2>
         {/* PROJECTS component */}
         <p className="mt-3 ">
-          Assistive Devices 3D Printing Library
-          <span className="block italic">
+          <span className="font-semibold">
+            Assistive Devices 3D Printing Library
+          </span>
+          <span className="block">
             Client: Alberta Health Services
           </span> 
-          <span className="">Role: Software Developer Team Lead</span>
+          <span className="">
+            Role: Software Developer, Agile Team Lead
+          </span>
+          <span className="block mt-5">
+            Tech Stack: Built with ASP.NET Blazor Framework with Microsoft SQL Server, Bootstrap, HTML/CSS, and JavaScript to create a robust and secure application.             
+          </span>
+          <span className="block mt-5">
+            Github/Repository Link: Due to the nature of the project, the repository is private, but I'd be happy to discuss the technical details, development process, and key contributions in an interview.            
+          </span>
+          <span className="block mt-5">
+          As Agile Team Lead, I led a team of developers through an iterative software development process, ensuring smooth collaboration and alignment with project goals. I facilitated team meetings and internal discussions, resolving roadblocks and guiding decision-making in cases of disagreement. Additionally, I managed client meetings, translating requirements into actionable development tasks. Alongside my leadership responsibilities, I was actively involved in both frontend and backend development, contributing significantly to the project's architecture and developing key microservices. Through fostering effective communication and keeping the team laser-focused, we successfully finished the project 25% ahead of schedule which allowed us to focus on polishing the project, exceeding expectations while maintaining high quality.
 
+          The project received high praise from stakeholders and event attendees during the presentation.
+
+
+          </span>
         </p>
-
       </div>
       {/* socials */}
       <div className="mt-6 text-xl w-1/2">
@@ -64,5 +79,8 @@ export default function Home() {
 
     </div>
     
+    // MOBILE
+
+
   );
 }
