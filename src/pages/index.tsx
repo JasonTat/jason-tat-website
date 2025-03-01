723dc34a526666303bfb87b0b3f7653cdf413b7e
JasonTat <112578835+JasonTat@users.noreply.github.com>
2025-02-28 22:24:19 -0700

    update to projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <div className="mt-6 text-xl w-1/2">
         <p className="italic text-2xl font-semibold mb-2">A little about me</p>
         <p className="mb-6">
-          I'm a Full Stack Developer with a passion for creating excellent and innovative projects. 
+          I'm a Full Stack Developer with a passion for creating excellent and innovative projects and collaboration. 
         </p>
         <p className="">
           I've always had a passion for technology, especially software. I grew up with computers and video games; the first time I've ever read, touched, and written code was back in 2010 during the peak era of Tumblr blogs, where I'd edit the HTML and CSS of my own blog page.  
@@ -29,7 +29,7 @@ export default function Home() {
         <p className="mt-6">
           What got me interested in pursuing software was working at TELUS as a Technical Support Representative. We'd use multiple pieces of software to analyze and diagnose a client's issue, and it was through doing that, that I became aware of how amazing software can be, and interested in how it looked and functioned under the hood, especially in an intricately connected system.
         </p>
-      </div>``
+      </div>
 
       {/* education */}
       <div className="mt-6 text-xl w-1/2">
@@ -40,39 +40,94 @@ export default function Home() {
         </p>
       </div>
 
+      {/* TECH STACK */}
+      <div className="mt-6 text-xl w-1/2">
+        <p className="italic text-2xl font-semibold mb-2">Technical Skills</p>
+        <ul className="ml-6">
+          <li>C#, Java, JavaScript, SQL</li>
+          <li>ASP.NET, React, TypeScript, Node.js, Next.js, Flutter</li>
+          <li>BootStrap, TailwindCSS, MaterialUI</li>
+          <li>SQL, MySQL, NoSQL</li>
+          <li>REST API</li>
+          <li>SSAS, SSRS, SSIS</li>
+          <li>VS Code, Visual Studio, CLI</li>
+          <li>Windows, Linux</li>
+        </ul>
+        
+
+        <div className="mt-4 ">
+          <h3 className="italic font-semibold">Currently learning:</h3>
+          <div className="ml-6">
+            <ul>
+              <li>Python</li>
+              <li>AngularJS</li>
+            </ul>
+          </div>
+        </div>
+       
+
+      </div>
+
       {/* projects / experience */}
       <div className="mt-6 text-xl w-1/2">
         <h2 className="italic text-2xl font-semibold mb-2">  
           Projects
         </h2>
         {/* PROJECTS component */}
-        <p className="mt-3 ">
-          <span className="font-semibold">
-            Assistive Devices 3D Printing Library
-          </span>
-          <span className="block">
-            Client: Alberta Health Services
-          </span> 
-          <span className="">
-            Role: Software Developer, Agile Team Lead
-          </span>
-          <span className="block mt-5">
-            Tech Stack: Built with ASP.NET Blazor Framework with Microsoft SQL Server, Bootstrap, HTML/CSS, and JavaScript to create a robust and secure application.             
-          </span>
-          <span className="block mt-5">
-            Github/Repository Link: Due to the nature of the project, the repository is private, but I'd be happy to discuss the technical details, development process, and key contributions in an interview.            
-          </span>
-          <span className="block mt-5">
-          As Agile Team Lead, I led a team of developers through an iterative software development process, ensuring smooth collaboration and alignment with project goals. I facilitated team meetings and internal discussions, resolving roadblocks and guiding decision-making in cases of disagreement. Additionally, I managed client meetings, translating requirements into actionable development tasks. Alongside my leadership responsibilities, I was actively involved in both frontend and backend development, contributing significantly to the project's architecture and developing key microservices. Through fostering effective communication and keeping the team laser-focused, we successfully finished the project 25% ahead of schedule which allowed us to focus on polishing the project, exceeding expectations while maintaining high quality.
+        <div>          
+          <p className="mt-3 ">
+            <span className="font-semibold">
+              Assistive Devices 3D Printing Library
+            </span>
+            <span className="block">
+              Client: Alberta Health Services
+            </span> 
+            <span className="">
+              Role: Software Developer, Agile Team Lead
+            </span>
+            <span className="block mt-5">
+              Tech Stack: Built with ASP.NET Blazor Framework with Microsoft SQL Server, Bootstrap, HTML/CSS, and JavaScript to create a robust and secure application.             
+            </span>
+            <span className="block mt-5">
+              Github/Repository Link: Due to the nature of the project, the repository is private, but I'd be happy to discuss the technical details, development process, and key contributions in an interview.            
+            </span>
+            <span className="block mt-5">
+              As Agile Team Lead, I led a team of developers through an iterative software development process, ensuring smooth collaboration and alignment with project goals. I facilitated team meetings and internal discussions, resolving roadblocks and guiding decision-making in cases of disagreement. 
 
-          The project received high praise from stakeholders and event attendees during the presentation.
 
+            </span>
+            <span className="block mt-5">
+              I managed client meetings, translating requirements into actionable development tasks. Alongside my leadership responsibilities, I was actively involved in both frontend and backend development, contributing significantly to the project's architecture and developing key microservices. Through fostering effective communication and keeping the team laser-focused, we successfully finished the project 25% ahead of schedule which allowed us to focus on polishing the project, exceeding expectations while maintaining a high quality product.
+            </span>
+            <span className="block mt-5">
+              The project received high praise from stakeholders and event attendees during the presentation.
+            </span>
+          </p>
+        </div>
+        <div>
+          <p className="mt-3">
+            <span className="font-semibold">
+              Cat Facts
+            </span>
+            <span className="block">
+              Client: Mobile App Development Course at School
+            </span> 
+            <span className="block mt-5">
+              Tech Stack: Dart, Flutter       
+            </span>
+            <span className="block mt-5">
+              Github/Repository Link: <a className="underline" href="https://github.com/JasonTat/2504-catproject">https://github.com/JasonTat/2504-catproject</a>
+            </span>
+          </p>
+        </div>
 
-          </span>
-        </p>
       </div>
+
+
+
+
       {/* socials */}
-      <div className="mt-6 text-xl w-1/2">
+      <div className="mt-6 mb-8 text-xl w-1/2">
         <p className="italic font-semibold mb-2">Contact Me!</p>
         <Socials />
       </div>
